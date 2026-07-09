4bfcbb9ffb411704a9d9d5d66b2775ad8c28884c
Aurelle Raviando <aurelle@berkeley.edu>
2026-07-09 05:03:35 +0000

    complete game logic yay

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -1,4 +1,5 @@
 class WordGuesserGame
+  attr_accessor :word, :guesses, :wrong_guesses
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/wordguesser_game_spec.rb pass.
 
@@ -6,6 +7,41 @@ class WordGuesserGame
 
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+  def guess(letter)
+    if letter.nil? || letter.empty? || letter !~ /^[a-zA-Z]$/
+    raise ArgumentError
+    end
+    letter = letter.downcase
+    if @guesses.include?(letter) || @wrong_guesses.include?(letter)
+    return false
+    end
+    if @word.include?(letter)
+    @guesses += letter
+    else
+    @wrong_guesses += letter
+  end
+  true
+  end
+  def word_with_guesses
+  @word.chars.map do |letter|
+    if @guesses.include?(letter)
+    letter
+    else
+    '-'
+    end
+  end.join
+  end
+  def check_win_or_lose
+  if word_with_guesses== @word
+    :win
+  elsif @wrong_guesses.length >= 7
+    :lose
+  else
+    :play
+  end
   end
 
   # You can test it by installing irb via $ gem install irb
